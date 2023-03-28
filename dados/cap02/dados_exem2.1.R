2d4b9d3bf295ae1ae5f562d436cfccf0b0fbf6f4
bendeivide <ben.deivide@gmail.com>
2023-03-28 16:37:31 -0300

    update

---
+++ dados/cap02/dados_exem2.1.R
@@ -0,0 +1,32 @@
+#=================================================================================
+# Código 2.1 - Os dados representam a temperatura (ºF) do anel de vedação de cada 
+#              teste de acionamento ou lançamento real do motor do foguete Challen
+#              ger
+#-----------
+# Como citar o livro:
+# BATISTA, B. D. O.. Estatística e Probabilidade: aplicadas às Engenharias e Ciên-
+# cias. Ouro Branco, MG: [s.n.]. 2023. XXX p.. ISBN 978-65-00-66079-1. Disponível 
+# em: <https://bendeivide.github.io/book-epaec/>. Acesso em: 
+#=================================================================================
+
+# Instalando o pacote 'leem' (via CRAN)
+install.packages("leem")
+# (ou) Instalando via github
+pkgs <- c("manipulate", "tkRplotR", 
+          "tkrplot", "crayon")
+install.packages(pkgs)
+## Instalando o pacote 'devtools'
+install.packages("devtools")
+## Instalando o pacote 'leem'
+devtools::install_github("bendeivide/leem")
+# Anexando o pacote leem
+library(leem)
+# Importando o banco de dados
+con <- url("https://raw.githubusercontent.com/bendeivide/book-epaec/master/dados/cap02/dados_exem2.1.RData")
+load(con); close(con)
+# Imprimindo os dados
+dados_exem2.1
+# Tabulando os dados
+dados_exem2.1 |>
+  new_leem(variable = 2) |>
+  tabfreq()
---
+++ dados/cap02/tabela2.1.R
@@ -0,0 +1,37 @@
+#=================================================================================
+# Tabela 2.1 - Dados brutos sobre o número de erros encontrados em 20 conjuntos de 
+#              caracteres monitorado em um canal de comunicação.
+#-----------
+# Como citar o livro:
+# BATISTA, B. D. O.. Estatística e Probabilidade: aplicadas às Engenharias e Ciên-
+# cias. Ouro Branco, MG: [s.n.]. 2023. XXX p.. ISBN 978-65-00-66079-1. Disponível 
+# em: <https://bendeivide.github.io/book-epaec/>. Acesso em: 
+#=================================================================================
+
+# Instalando o pacote 'leem' (via CRAN)
+install.packages("leem")
+# (ou) Instalando via github
+pkgs <- c("manipulate", "tkRplotR", 
+          "tkrplot", "crayon")
+install.packages(pkgs)
+## Instalando o pacote 'devtools'
+install.packages("devtools")
+## Instalando o pacote 'leem'
+devtools::install_github("bendeivide/leem")
+# Anexando o pacote leem
+library(leem)
+# Importando o banco de dados
+con <- url("https://raw.githubusercontent.com/bendeivide/book-epaec/master/dados/cap02/dados_exem2.1.RData")
+load(con); close(con)
+# Imprimindo os dados
+dados_exem2.1
+# Tabulando os dados
+dados_exem2.1 |>
+  new_leem(variable = 2) |>
+  tabfreq()
+
+
+con <- read.table("https://raw.githubusercontent.com/bendeivide/book-epaec/master/dados/cap02/nerros.txt", h = T)
+nerros <- con$erros
+
+save(nerros, file = "tabela2.1.RData")
